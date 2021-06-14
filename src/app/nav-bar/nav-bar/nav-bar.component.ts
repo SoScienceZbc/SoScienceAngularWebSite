0712b9648c3698905c945a7e649754f198a63139
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2021-06-14 08:38:53 +0200

    lmkui

---
+++ src/app/nav-bar/nav-bar/nav-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSidenav } from '@angular/material/sidenav';
+
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  navbarBurgermenuopen : boolean = false;
+
+  constructor(private route:Router,public sidenav:MatSidenav) { }
+
+  ngOnInit(): void {
+  }
+
+  logout(){
+    sessionStorage.clear();
+  }
+  toggle(){
+
+  }
+}
