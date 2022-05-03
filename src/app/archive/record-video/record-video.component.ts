510cf72a2deb98f66e5c45a7ea6f38723c2cb6c8
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2022-05-03 15:42:59 +0200

    Update record-video.component.ts

--- src/app/archive/record-video/record-video.component.ts
+++ src/app/archive/record-video/record-video.component.ts
@@ -69,6 +69,9 @@ export class RecordVideoComponent implements AfterViewInit{
     }
   }
   CloseDialog(){
+    if(this.mediaStream){
+      this.mediaStream.stop();
+    }
     this.dialog.closeAll();
   }
 
