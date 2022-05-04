5ba93a99d757ed5c64b6df1d49283414f47afa9d
Alexandar Lackovic Hansen <alex303a@zbc.dk>
2022-05-04 09:30:36 +0200

    d

--- src/app/archive/record-video/record-video.component.ts
+++ src/app/archive/record-video/record-video.component.ts
@@ -6,6 +6,7 @@ import { MediaStreamDirective } from './mediastreamDirective/media-stream.direct
 import { DatabaseService } from 'src/app/database.service';
 import { MediaServiceService } from 'src/app/media-service.service';
 import { MediaRequest } from 'src/app/protos/RemoteMediaProto_pb';
+import internal from 'stream';
 
 @Component({
   selector: 'app-record-video',
@@ -16,14 +17,15 @@ export class RecordVideoComponent implements AfterViewInit{
 
   @ViewChild(MediaStreamDirective)
   public mediaStream!: MediaStreamDirective;
-  // @Input('projectid') projectid!:number;
 
   public videoSrc!: SafeUrl;
   public videoBlob = {} as Blob;
+  public saved = {} as boolean;
   constructor(@Inject(MAT_DIALOG_DATA) public projectid: any, private dialog: MatDialog, private sanitizer: DomSanitizer, private mediaService: MediaServiceService) { }
 
   ngAfterViewInit(): void {
     this.mediaStream.play();
+    this.saved = true;
 
   }
   public onVideo(data: Blob): void {
@@ -41,9 +43,11 @@ export class RecordVideoComponent implements AfterViewInit{
     this.mediaStream.stop();
     console.log(this.videoSrc)
     console.log("projectid" + this.projectid.projectid);
+    this.saved = false;
   }
   clearRecording(){
     this.videoSrc = "";
+    this.saved = true;
   }
   async save(){
     if(this.videoBlob) {
@@ -66,6 +70,8 @@ export class RecordVideoComponent implements AfterViewInit{
       this.videoSrc = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(blob))
 
       this.mediaService.AddRecordedMedia(newVid);
+      this.saved = true;
+
     }
   }
   CloseDialog(){
