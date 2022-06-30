76590acc19cad72c2a6f9eff8d1afd06f060d2e2
Morten Vestergaard <mort286f@zbc.dk>
2022-06-30 15:28:00 +0200

    Added media type check for displaying video/audio

--- src/app/archive/media-record/display-media-file/display-media-file.component.ts
+++ src/app/archive/media-record/display-media-file/display-media-file.component.ts
@@ -19,13 +19,14 @@ export class DisplayMediaFileComponent implements AfterViewInit {
 
   public title = "";
   public videoSrc!: SafeUrl;
+  public mediaType = "";
 
 
   ngAfterViewInit(): void {
     //this.LoadContent();
   }
 
-  /*  
+  /*
     Loads the video from the database, sets the title to the video title in the database
     And the video blob retrieved as a Uint8 Array, being made into a new blob as video source
   */
@@ -36,6 +37,7 @@ export class DisplayMediaFileComponent implements AfterViewInit {
     let tempArray: Uint8Array;
     this.mediaservice.GetMedia(this.data.mediaid).subscribe( x => {
       tempArray = x.getRetrievedblobdata_asU8()
+      this.mediaType = x.getType();
       if(tempArray.length > 0) {
         var blob = new Blob([tempArray], {})
         this.videoSrc = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(blob))
